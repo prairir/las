d366aab7c63a8b46c04ac0da7ae8ea9ec60566d3
Ryan Prairie <me@ryanprairie.com>
2023-08-09 02:41:40 -0400

    Restart: basically restarting
    Im going to yoink and adapt parts of the old code but this is basically
    a whole rewrite so lets think of it that way.

--- src/ls.zig
+++
@@ -1,7 +0,0 @@
-const std = @import("std");
-const fs = std.fs;
-const Dir = fs.Dir;
-
-pub fn run(dir: Dir) anyerror!void {
-    std.log.info("ls: {*}", .{&dir});
-}
--- src/main.zig
+++ src/main.zig
@@ -1,50 +1,26 @@
 const std = @import("std");
 const io = std.io;
-const os = std.os;
 
 const clap = @import("clap");
 
-const las = @import("las.zig");
-
-const log = @import("log.zig");
-
-const parsers = .{
-    .FILE = clap.parsers.string,
-};
-
-pub fn main() anyerror!void {
+pub fn main() !void {
     const params = comptime clap.parseParamsComptime(
         \\-h, --help	Displays this message
         \\<FILE>
     );
 
-    const res = try clap.parse(clap.Help, &params, parsers, .{});
-
-    if (res.args.help) {
-        try log.errWriter.writeAll("Usage: las ");
-        try clap.usage(log.errWriter, clap.Help, &params);
-        try log.errWriter.writeAll("\n\nOptions:\n");
-        try clap.help(log.errWriter, clap.Help, &params, .{});
-        std.os.exit(0);
-    }
-
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-
-    var files = std.ArrayList([]const u8).init(allocator);
-    defer files.deinit();
-    if (res.positionals.len == 0) {
-        const files_arr = [_]u8{'.'};
-        try files.append(files_arr[0..]);
-    } else {
-        for (res.positionals) |pos| {
-            try files.append(pos);
-        }
-    }
-
-    las.run(allocator, files.toOwnedSlice()) catch |err| {
-        try log.errWriter.print("las: ERROR: \"{s}\"", .{@errorName(err)});
-        os.exit(1);
+    const parsers = comptime .{
+        .FILE = clap.parsers.string,
     };
-    os.exit(0);
+
+    var res = try clap.parse(clap.Help, &params, parsers, .{});
+    defer res.deinit();
+
+    if (res.args.help != 0) {
+        return clap.help(std.io.getStdErr().writer(), clap.Help, &params, .{});
+    }
+
+    for (res.positionals) |pos| {
+        std.debug.print("{s}\n", .{pos});
+    }
 }
