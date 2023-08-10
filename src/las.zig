d1efa926b09dcddaa28a2b0b6e8d28449f36f7bc
Ryan Prairie <me@ryanprairie.com>
2023-08-10 02:54:02 -0400

    Add: AT_FDCWD const, Add: basic stat mode switch
    doing the switching on the mode is basically required. the old
    alternative i used was to try to open and fail then catch.
    Thats *weird*, i dont know what i was thinking. anyway this lets us do
    modules and more stuff in the future if we want.

--- src/las.zig
+++ src/las.zig
@@ -3,11 +3,25 @@ const os = std.os;
 
 const Allocator = std.mem.Allocator;
 
+const AT_FDCWD = -100;
+
 pub fn run(allocator: Allocator, paths: [][]const u8) anyerror!void {
     _ = allocator;
 
     for (paths) |path| {
-        const stat = try os.fstatat(-100, path, 0);
+        const stat = try os.fstatat(AT_FDCWD, path, 0);
         std.debug.print("{d}\n", .{stat.mode});
+
+        switch (stat.mode & std.os.S.IFMT) {
+            std.os.linux.S.IFREG => {
+                std.debug.print("REGULAR FILE CAT CAT\n", .{});
+            },
+            std.os.linux.S.IFDIR => {
+                std.debug.print("REGULAR DIR LS LS\n", .{});
+            },
+            else => {
+                std.debug.print("WHO KNOWS\n", .{});
+            },
+        }
     }
 }
